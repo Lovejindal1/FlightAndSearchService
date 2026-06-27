5278fd6148b5e2fc57473a1c8e4a0307e6827c06
Love Jindal <lkjindal123@gmail.com>
2026-06-27 11:57:46 +0530

    Added unique and started cityrepository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const {PORT} = require('./config/serverConfig');
+
 const setupAndStartServer = async()=>{
 
     // create the express object
@@ -11,8 +13,7 @@ const setupAndStartServer = async()=>{
     
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
-        
-    })
+    });
 }
 
 setupAndStartServer(); 
--- src/models/city.js
+++ src/models/city.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class City extends Model {
     /**
@@ -14,7 +12,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   City.init({
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    allowNull: false,
+    unique:true
   }, {
     sequelize,
     modelName: 'City',

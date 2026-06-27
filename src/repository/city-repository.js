557e2e85fb2383bc93940cca6a3b4832f39c0c60
Love Jindal <lkjindal123@gmail.com>
2026-06-27 21:32:53 +0530

    Added more routers for city api

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -13,7 +13,7 @@ class CityRepository {
         }
     }
     
-    async deleteCity({cityId}){
+    async deleteCity(cityId){
         try{
             await City.destroy({
                 where:{

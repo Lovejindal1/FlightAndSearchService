1ca4496a81e6c442f12bdce582c148cb4419daac
Love Jindal <lkjindal123@gmail.com>
2026-06-29 02:25:33 +0530

    Added airplane

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+// const {Airplane} = require('./models/index');
+
 const setupAndStartServer = async()=>{
 
     // create the express object

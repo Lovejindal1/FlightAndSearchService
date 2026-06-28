d5d33054c7dc307636a14c0932b9c4321f7ef144
Love Jindal <lkjindal123@gmail.com>
2026-06-28 20:14:35 +0530

    Added aiport seeders

--- src/index.js
+++ src/index.js
@@ -12,10 +12,13 @@ const setupAndStartServer = async()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     
-    app.use('/api',ApiRoutes);
+    app.use('/api',ApiRoutes);   
 
     app.listen(PORT, async ()=>{
         console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
     });
 }
 
